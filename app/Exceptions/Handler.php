9b4e6384a2a94c6f171e9c8fe565059ea9026d63
fekoh <mbj/4592>
2019-11-21 18:10:03 +0100

    Setup Error handlers for json requests

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,10 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +49,69 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($request->ajax() || $request->wantsJson()) {
+            return $this->getJsonResponse($exception);
+        }
+        
         return parent::render($request, $exception);
+    }
+
+    protected function getJsonResponse(Exception $exception)
+    {
+        $debugEnabled = config('app.debug');
+
+        $exception = $this->prepareException($exception);
+
+        /*
+         * Handle validation errors thrown using ValidationException.
+         */
+        if ($exception instanceof ValidationException) {
+
+            $validationErrors = $exception->validator->errors()->all();
+
+            /*
+             * Laravel validation error format example
+             * "attribute" => [
+             *      "The attribute failed validation."
+             * ]
+             *
+             * What we need as per the api spec
+             * "attribute" => [
+             * 	    "failed validation."
+             * ]
+             */
+
+            return response()->json(['errors' => $validationErrors], 422);
+        }
+
+        /*
+         * Handle database errors thrown using QueryException.
+         * Prevent sensitive information from leaking in the error message.
+         */
+        if ($exception instanceof QueryException) {
+            if ($debugEnabled) {
+                $message = $exception->getMessage();
+            } else {
+                $message = 'DB Error: ';
+            }
+        }
+
+        $statusCode = $this->getStatusCode($exception);
+
+        if (! isset($message) && ! ($message = $exception->getMessage())) {
+            $message = sprintf('%d %s', $statusCode, Response::$statusTexts[$statusCode]);
+        }
+
+        $errors = [
+            'message' => $message,
+            'status_code' => $statusCode,
+        ];
+
+        if ($debugEnabled) {
+            $errors['exception'] = get_class($exception);
+            $errors['trace'] = explode("\n", $exception->getTraceAsString());
+        }
+
+        return response()->json(['errors' => $errors], $statusCode);
     }
 }
